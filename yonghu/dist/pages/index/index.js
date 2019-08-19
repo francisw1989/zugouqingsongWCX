78d75e8a57a0db03be2e66c904842cf272fd7c44
francisw1989 <838883685@qq.com>
2019-08-19 20:28:07 +0800

    32

--- yonghu/dist/pages/index/index.js
+++ yonghu/dist/pages/index/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.default = Page({
-  data: {
-    imgUrls: ['https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1566113254&di=4ee1a56ba1727f36fb136bd18d4eed12&src=http://b-ssl.duitang.com/uploads/item/201808/27/20180827030547_tsblg.jpg', 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1566113254&di=4ee1a56ba1727f36fb136bd18d4eed12&src=http://b-ssl.duitang.com/uploads/item/201808/27/20180827030547_tsblg.jpg']
-  }
+    data: {}
 });
