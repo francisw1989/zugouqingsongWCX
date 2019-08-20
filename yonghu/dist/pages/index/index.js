6c5432726eb23cd0c8b2c0bbf32c8cc9e8b79a06
wangmiao <wangmiao@yunzhangfang.com>
2019-08-20 21:08:51 +0800

    '32'

--- yonghu/dist/pages/index/index.js
+++ yonghu/dist/pages/index/index.js
@@ -4,5 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = Page({
-    data: {}
+    data: {
+        jxzShow: false,
+        lcList: [{ time: '', content: '预约开始时间：2019-06-11 12:00' }, { time: '', content: '预约时长：50分钟' }, { time: '', content: '预计结束时间：2019-06-11 12:50' }]
+    }
 });
