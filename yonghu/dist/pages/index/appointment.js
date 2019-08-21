fbd4e53f983400fce66911369437fbec8217cd93
francisw1989 <838883685@qq.com>
2019-08-21 09:43:30 +0800

    23

---
+++ yonghu/dist/pages/index/appointment.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = Page({
+    data: {
+        time: '',
+        cIndex: '-1'
+    },
+    chose: function chose(e) {
+        console.log('323');
+        var t = this;
+        t.setData({
+            cIndex: e.target.dataset.index
+        });
+    },
+    bindTimeChange: function bindTimeChange() {}
+});
