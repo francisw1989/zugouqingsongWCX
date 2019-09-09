6b21a9fc6856a9860fa2ef85581d63e60d0b61f2
francisw1989 <838883685@qq.com>
2019-09-09 15:30:36 +0800

    32

--- yonghu/dist/pages/wode/index.js
+++ yonghu/dist/pages/wode/index.js
@@ -1,8 +1,18 @@
 'use strict';
-
+const app = getApp();
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = Page({
-    data: {}
+    data: {
+        userInfo: {},
+        memberLevelNameList: ['普通会员','一星会员','二星会员','三星会员']
+    },
+    onLoad(){
+        const t = this;
+        let userInfo = app.globalData.userInfo;
+        t.setData({
+            userInfo: userInfo
+        })
+    }
 });
