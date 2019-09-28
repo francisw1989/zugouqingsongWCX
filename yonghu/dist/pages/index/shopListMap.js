1d45313f8506df80110affb19ccb6d3cec85136b
francisw1989 <838883685@qq.com>
2019-09-28 12:01:06 +0800

    32

--- yonghu/dist/pages/index/shopListMap.js
+++ yonghu/dist/pages/index/shopListMap.js
@@ -105,7 +105,7 @@ exports.default = Page({
         });
         // 设置中心点
         t.setData({
-            item: app.globalData.stores[0]
+            item: app.globalData.stores[2]
         })
         console.log(t.data.item)
         t.setData({
