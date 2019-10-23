0a60df052f792da44ab93e1d92b3f1eac5c78985
francisw1989 <838883685@qq.com>
2019-10-23 11:03:38 +0800

    32

--- yonghu/dist/pages/wode/memberGrade.js
+++ yonghu/dist/pages/wode/memberGrade.js
@@ -5,22 +5,77 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = Page({
     data: {
-        value1: 452,
-        value2: 5,
-        memberLevelName: app.globalData.memberLevelName,
-        thumbStyle: {
-            'width': '18px',
-            'height': '18px',
-            'border-radius': '9px',
-            'background-color': '#B99063',
-            'top': '6px'
-
-        }
+        list: [
+            {
+                "createTime": "09-19 11:54",
+                "createBy": null,
+                "updateTime": "2019-10-16 15:44",
+                "id": 1,
+                "memberLevel": 0,
+                "interestsContent": "免费送一次足底按摩（30分钟）",
+                "memberLevelName": "初级会员",
+                "amountCondition": 500,
+                "frequencyCondition": 10,
+                "changebackScale": 10,
+                "icon": null
+            },
+            {
+                "createTime": "09-19 11:55",
+                "createBy": null,
+                "updateTime": "2019-10-16 15:44",
+                "id": 2,
+                "memberLevel": 1,
+                "interestsContent": "免费送一次颈部放松（60分钟）",
+                "memberLevelName": "一星会员",
+                "amountCondition": 1000,
+                "frequencyCondition": 20,
+                "changebackScale": 8,
+                "icon": null
+            },
+            {
+                "createTime": "09-19 11:55",
+                "createBy": null,
+                "updateTime": "2019-10-16 15:44",
+                "id": 3,
+                "memberLevel": 2,
+                "interestsContent": "免费送一次足底和spa（30分钟）",
+                "memberLevelName": "二星会员",
+                "amountCondition": 2000,
+                "frequencyCondition": 40,
+                "changebackScale": 4,
+                "icon": null
+            },
+            {
+                "createTime": "09-19 11:55",
+                "createBy": null,
+                "updateTime": "2019-10-16 15:44",
+                "id": 4,
+                "memberLevel": 3,
+                "interestsContent": "免费送小食拼盘一个（10次）\n免费送小食拼盘一个（10次）",
+                "memberLevelName": "三星会员",
+                "amountCondition": 5100,
+                "frequencyCondition": 90,
+                "changebackScale": 2,
+                "icon": null
+            }
+        ],
+        inkBarStyle: {
+            'width': '30%'
+        },
+        current: 0
     },
     onShow(){
         const t = this;
         t.setData({
             userInfo: app.globalData.userInfo
         })
-    }
+    },
+    tabclick(e) {
+        const t = this;
+        var index = e.currentTarget.dataset.index;
+        this.setData({
+            current: index
+        });
+        
+    },
 });
