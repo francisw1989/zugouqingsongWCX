c24f36044d348105aa794b3ee4d83e958b158b28
francisw1989 <838883685@qq.com>
2019-11-06 18:41:27 +0800

    32

--- yonghu/dist/pages/index/chooseProjectJishi.js
+++ yonghu/dist/pages/index/chooseProjectJishi.js
@@ -240,9 +240,8 @@ exports.default = Page({
         const t = this;
         let i = e.target.dataset.index;
         let obj = t.data.chooseProject[t.data.cIndex].technicianList[i];
-        debugger
         app.globalData.chooseStore.appointTime = app.formatDate(new Date(app.globalData.chooseStore.appointTime).getTime() + obj.waitTime * 60 * 1000);
-        t.waitId = obj.id;
+        t.data.waitId = obj.id;
         t.onLoad();
     },
     clearTechnician(e){
