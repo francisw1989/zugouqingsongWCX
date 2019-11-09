99bd4883ca58e276deb20a71693742671bda5eb2
francisw1989 <838883685@qq.com>
2019-11-09 16:50:34 +0800

    32

--- yonghu/dist/pages/wode/about.js
+++ yonghu/dist/pages/wode/about.js
@@ -1,4 +1,5 @@
 // pages/wode/about.js
+const app = getApp();
 Page({
 
     /**
@@ -12,7 +13,12 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        const t = this;
+        app.getSysItemDetail().then((res)=>{
+            t.setData({
+                D: res
+            })
+        })
     },
 
     /**
