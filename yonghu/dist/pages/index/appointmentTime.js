42f5216f1b2ac59d2f34c80b789080afbbd888c7
francisw1989 <838883685@qq.com>
2019-11-16 17:36:28 +0800

    32

--- yonghu/dist/pages/index/appointmentTime.js
+++ yonghu/dist/pages/index/appointmentTime.js
@@ -49,9 +49,9 @@ exports.default = Page({
 
 
         if (nowMin <= 30){
-            t.data.mins = [ '30']
+            t.data.mins = [ '30', '40', '50']
         } else if (nowMin > 30){
-            t.data.mins = ['00', '30']
+            t.data.mins = ['00', '10', '30', '40', '50']
         }
         
         t.setData({
@@ -65,15 +65,15 @@ exports.default = Page({
         const val = e.detail.value;
         if (val[0] > 0){
             t.setData({
-                mins : ['00', '30']
+                mins: ['00', '10', '30', '40', '50']
             })
         }else{
             let now = new Date();
             let nowMin = now.getMinutes();
             if (nowMin <= 30) {
-                t.data.mins = ['30']
+                t.data.mins = ['30', '40', '50']
             } else if (nowMin > 30) {
-                t.data.mins = ['00', '30']
+                t.data.mins = ['00', '10', '30', '40', '50']
             }
             t.setData({
                 mins: t.data.mins
