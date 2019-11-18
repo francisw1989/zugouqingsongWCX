e75843e32b6a75f661d4e6546479575c5ead5201
francisw1989 <838883685@qq.com>
2019-11-18 15:30:36 +0800

    32

--- yonghu/dist/pages/index/pay.js
+++ yonghu/dist/pages/index/pay.js
@@ -18,7 +18,7 @@ exports.default = Page({
         couponName: '',
         userInfo: {}
     },
-    bindtimeup(){
+    bindtimeup() {
         wx.switchTab({
             url: '../index/index'
         })
@@ -39,7 +39,7 @@ exports.default = Page({
         // 3.余额 >= 订单总价时  选择了余额支付  则不可以选择微信支付。
 
         // 余额足
-        if (t.data.U.totalAccount >= t.data.D.totalPrice){
+        if (t.data.U.totalAccount >= t.data.D.totalPrice) {
             if (e.detail.value[e.detail.value.length - 1] == 1) {
                 t.setData({
                     wxChecked: false,
@@ -51,79 +51,79 @@ exports.default = Page({
                     yeChecked: false
                 })
             }
-        }else{
+        } else {
             // 余额不足
             t.setData({
                 wxChecked: true
             })
         }
     },
-    orderPay(){
+    orderPay() {
         const t = this;
-      console.log(t.data.wxChecked);
-      if (!t.data.wxChecked && !t.data.yeChecked) {
+        console.log(t.data.wxChecked);
+        if (!t.data.wxChecked && !t.data.yeChecked) {
             wx.showModal({
                 title: '提示',
                 content: '请选择支付方式',
             })
             return
         }
-      if (t.data.wxChecked && t.data.yeChecked) {
+        if (t.data.wxChecked && t.data.yeChecked) {
             t.setData({
                 type: 0
             })
-        }else{
-        if (t.data.yeChecked) {
+        } else {
+            if (t.data.yeChecked) {
                 t.setData({
                     type: 1
                 })
             }
-        if (t.data.wxChecked){
+            if (t.data.wxChecked) {
                 t.setData({
                     type: 2
                 })
             }
-            
-        }
-        if (t.data.pageFrom == 'goods'){
-            app.articleOrderPay(t.data.type).then((res)=>{
-                app.globalData.wxObj = res;
-              if (res=='' || res == null || res.needWxPay == 0) {
-                wx.showModal({
-                  title: '提示',
-                  content: '支付成功，即将返回首页！',
-                  success(res) {
-                    if (res.confirm) {
-                      wx.switchTab({
-                        url: 'index',
-                      })
-                    } else if (res.cancel) {
-                      console.log('用户点击取消')
-                    }
-                  }
-                })
-              }
-              if (res!='' && res!=null && res.appId!=null) {
-                app.wxPay().then(() => {
-                  wx.showModal({
-                    title: '提示',
-                    content: '支付成功，即将返回首页！',
-                    success(res) {
-                      if (res.confirm) {
-                        wx.switchTab({
-                          url: 'index',
-                        })
-                      } else if (res.cancel) {
-                        console.log('用户点击取消')
-                      }
-                    }
-                  })
 
-                });
-              }
-                
+        }
+        if (t.data.pageFrom == 'goods') {
+            app.articleOrderPay(t.data.type).then((res) => {
+                app.globalData.wxObj = res;
+                if (res == '' || res == null || res.needWxPay == 0) {
+                    wx.showModal({
+                        title: '提示',
+                        content: '支付成功，即将返回首页！',
+                        success(res) {
+                            if (res.confirm) {
+                                wx.switchTab({
+                                    url: 'index',
+                                })
+                            } else if (res.cancel) {
+                                console.log('用户点击取消')
+                            }
+                        }
+                    })
+                }
+                if (res != '' && res != null && res.appId != null) {
+                    app.wxPay().then(() => {
+                        wx.showModal({
+                            title: '提示',
+                            content: '支付成功，即将返回首页！',
+                            success(res) {
+                                if (res.confirm) {
+                                    wx.switchTab({
+                                        url: 'index',
+                                    })
+                                } else if (res.cancel) {
+                                    console.log('用户点击取消')
+                                }
+                            }
+                        })
+
+                    });
+                }
+
             })
-        }else{
+        } else {
             app.orderPay(t.data.type).then((res) => {
                 if (res.needWxPay == 1) {
                     app.globalData.wxObj = res.data;
@@ -147,10 +147,10 @@ exports.default = Page({
 
             })
         }
-        
+
     },
-    
-    onLoad(opt){
+
+    onLoad(opt) {
         const t = this;
         // 1.余额为0时，只能选择微信支付    
         // 2.余额 > 0且小于订单总价时，选择账户支付 同时必须选择微信支付   
@@ -164,14 +164,14 @@ exports.default = Page({
             D: app.globalData.orderDetail,
             count: opt.count
         })
-        if (app.globalData.orderDetail.isCoupon == 1 || app.globalData.orderDetail.isAssemble == 1){
-          t.setData({
-            couponName: app.globalData.orderDetail.coupon.couponName
-          })
-        }else{
-          t.setData({
-            couponName: ''
-          })
+        if (app.globalData.orderDetail.isCoupon == 1 || app.globalData.orderDetail.isAssemble == 1) {
+            t.setData({
+                couponName: app.globalData.orderDetail.coupon.couponName
+            })
+        } else {
+            t.setData({
+                couponName: ''
+            })
         }
 
         // if (t.data.U.totalAccount < t.data.D.totalPrice) {
@@ -181,18 +181,18 @@ exports.default = Page({
         //     })
         // }
     },
-    onShow(){
+    onShow() {
         const t = this;
-        if (app.globalData.chooseCoupon){
+        if (app.globalData.chooseCoupon) {
             t.setData({
                 couponName: app.globalData.chooseCoupon.couponName,
             })
-            app.checkPrice().then(()=>{
+            app.checkPrice().then(() => {
                 t.setData({
                     D: app.globalData.orderDetail
                 })
             })
-            
+
         }
         if (wx.getStorageSync('openId')) {
             app.userInfo(true).then(() => {
@@ -203,23 +203,23 @@ exports.default = Page({
         }
     },
     //取消优惠券
-    cancelCoupon(){
-      const t = this;
-      app.globalData.chooseCoupon = null;
-      app.cancelCoupon().then(() => {
-        console.log(app.globalData.orderDetail);
-        t.setData({
-          D: app.globalData.orderDetail
+    cancelCoupon() {
+        const t = this;
+        app.globalData.chooseCoupon = null;
+        app.cancelCoupon().then(() => {
+            console.log(app.globalData.orderDetail);
+            t.setData({
+                D: app.globalData.orderDetail
+            });
+            if (app.globalData.orderDetail.isCoupon == 1 || app.globalData.orderDetail.isAssemble == 1) {
+                t.setData({
+                    couponName: app.globalData.orderDetail.coupon.couponName
+                })
+            } else {
+                t.setData({
+                    couponName: ''
+                })
+            }
         });
-        if (app.globalData.orderDetail.isCoupon == 1 || app.globalData.orderDetail.isAssemble == 1) {
-          t.setData({
-            couponName: app.globalData.orderDetail.coupon.couponName
-          })
-        } else {
-          t.setData({
-            couponName: ''
-          })
-        }
-      });
     }
 });
